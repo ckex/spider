e71b254df9ed208825899292b07a494a2ad28894
Ckex.zha <ckex868@vip.qq.com>
2016-12-05 09:32:37 +0800

    change biadumobile test

--- mljr-spider/src/main/java/com/mljr/spider/storage/HttpPipeline.java
+++ mljr-spider/src/main/java/com/mljr/spider/storage/HttpPipeline.java
@@ -21,14 +21,12 @@ import org.apache.http.concurrent.FutureCallback;
 import org.apache.http.entity.ByteArrayEntity;
 import org.apache.http.entity.ContentType;
 import org.apache.http.util.EntityUtils;
-import org.aspectj.weaver.NewConstructorTypeMunger;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.google.common.base.Stopwatch;
 import com.mljr.spider.http.AsyncHttpClient;
 
-import net.sf.ehcache.util.counter.Counter;
 import us.codecraft.webmagic.ResultItems;
 import us.codecraft.webmagic.Task;
 import us.codecraft.webmagic.pipeline.Pipeline;
@@ -89,14 +87,16 @@ public class HttpPipeline implements Pipeline {
 				try {
 					watch.stop();
 					int code = result.getStatusLine().getStatusCode();
-					if (code != 200) {
+					String response = EntityUtils.toString(result.getEntity());
+					// response 不以 0 开头则为失败
+					if (code != 200 || !StringUtils.startsWith(response, "0")) {
 						COUNTER.failure.incrementAndGet();
 						if (flag.compareAndSet(true, false)) {
 							standbyPipeline.process(items, t);
 						}
 					}
 					logger.debug("response code:" + code + ",useTime " + watch.elapsed(TimeUnit.MILLISECONDS) + ","
-							+ COUNTER.toString());
+							+ COUNTER.toString() + " response:" + response);
 				} catch (Exception e) {
 					if (logger.isDebugEnabled()) {
 						e.printStackTrace();
@@ -153,11 +153,18 @@ public class HttpPipeline implements Pipeline {
 			originalContent.writeTo(gzipOut);
 			gzipOut.finish();
 			post.setEntity(new ByteArrayEntity(baos.toByteArray(), contentType));
+
+			// post.setEntity(EntityBuilder.create().setContentEncoding("UTF-8").setText(html).setContentType(contentType)
+			// .gzipCompress().build());
+			COUNTER.num.incrementAndGet();
+			httpclient.post(post, callback, 3000);
+			return true;
 		} catch (Exception e) {
 			if (logger.isDebugEnabled()) {
 				e.printStackTrace();
 			}
 			logger.error(ExceptionUtils.getStackTrace(e));
+			return false;
 		} finally {
 			try {
 				if (gzipOut != null) {
@@ -178,11 +185,6 @@ public class HttpPipeline implements Pipeline {
 			} catch (IOException e) {
 			}
 		}
-		// post.setEntity(EntityBuilder.create().setContentEncoding("UTF-8").setText(html).setContentType(contentType)
-		// .gzipCompress().build());
-		COUNTER.num.incrementAndGet();
-		httpclient.post(post, callback, 3000);
-		return true;
 	}
 
 }
