ed2cd82d66d40eee4acbc4f50c67535902194835
chi.song <chi.song@mljr.com>
2016-12-06 13:24:55 +0800

    IP138 scheduler

---
+++ mljr-spider/src/main/java/com/mljr/spider/scheduler/IP138Scheduler.java
@@ -0,0 +1,63 @@
+/**
+ * BaiduMobileScheduler.java by Ckex.zha
+ */
+package com.mljr.spider.scheduler;
+
+import com.google.common.base.CharMatcher;
+import com.mljr.spider.mq.UMQMessage;
+import com.mljr.spider.scheduler.manager.AbstractMessage.PullMsgTask;
+import us.codecraft.webmagic.Request;
+import us.codecraft.webmagic.Spider;
+import us.codecraft.webmagic.Task;
+
+import java.util.concurrent.BlockingQueue;
+
+/**
+ * @author Ckex.zha <br/>
+ *         Nov 17, 20163:06:26 PM
+ */
+public class IP138Scheduler extends AbstractScheduler {
+
+	private static final String URL = "http://www.ip138.com:8080/search.asp?mobile=%s&action=mobile";
+
+	public IP138Scheduler(Spider spider, PullMsgTask task) throws Exception {
+		super(spider, task);
+	}
+
+	public IP138Scheduler(Spider spider, String qid) throws Exception {
+		super(spider, qid);
+	}
+
+	public IP138Scheduler(Spider spider, BlockingQueue<UMQMessage> queue) throws Exception {
+		super(spider, queue);
+	}
+
+	@Override
+	public void push(Request request, Task task) {
+		put(request);
+	}
+
+	@Override
+	public Request poll(Task task) {
+		return take();
+	}
+
+	@Override
+	public int getLeftRequestsCount(Task task) {
+		return 0;
+	}
+
+	@Override
+	public int getTotalRequestsCount(Task task) {
+		return 0;
+	}
+
+	@Override
+	public boolean pushTask(Spider spider, UMQMessage message) {
+		String url = String.format(URL, message.message);
+		url = CharMatcher.WHITESPACE.replaceFrom(CharMatcher.anyOf("\r\n\t").replaceFrom(url, ""), "");
+		push(new Request(url), spider);
+		return true;
+	}
+
+}
