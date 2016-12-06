607a2765181dce66704a77b160b4352cb05cd5d7
chi.song <chi.song@mljr.com>
2016-12-06 18:40:22 +0800

    qichacha processor

--- mljr-spider/src/main/java/com/mljr/spider/processor/QichachaProcessor.java
+++ mljr-spider/src/main/java/com/mljr/spider/processor/QichachaProcessor.java
@@ -1,37 +1,75 @@
 package com.mljr.spider.processor;
 
+import com.mljr.spider.storage.LocalFilePipeline;
+import org.apache.commons.lang3.StringUtils;
 import us.codecraft.webmagic.Page;
 import us.codecraft.webmagic.Site;
 import us.codecraft.webmagic.Spider;
-
-import java.util.List;
+import us.codecraft.webmagic.pipeline.ConsolePipeline;
+import us.codecraft.webmagic.pipeline.JsonFilePipeline;
+import us.codecraft.webmagic.selector.Html;
+import us.codecraft.webmagic.selector.Selectable;
 
 /**
  * @author songchi
  */
 public class QichachaProcessor extends AbstractPageProcessor {
 
-    private Site site;
+    private Site site = Site.me().setDomain("www.qichacha.com/").setRetrySleepTime(1500).setRetryTimes(3).setUserAgent(
+            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36");
+
+    public QichachaProcessor() {
+        super();
+    }
 
     @Override
     public void process(Page page) {
-        List<String> requests = page.getHtml().links().regex("http://www\\.qichacha\\.com/firm_.*").all();
-        page.addTargetRequests(requests);
-        page.putField("", page.getHtml());
+        Html html = page.getHtml();
+        page.putField("公司名称", html.xpath("//span[@class='text-big font-bold']/text()"));
+        page.putField("电话", html.xpath("//*[@id=\"company-top\"]/div/div[1]/span[2]/small[1]/text()"));
+        page.putField("邮箱", html.xpath("//*[@id=\"company-top\"]/div/div[1]/span[2]/small[1]/a[1]/text()"));
+
+        Selectable div = html.xpath("//[@class='panel-body text-sm']/ul[@class='company-base']");
+
+        String firstItem = div.xpath("//li[1]/label/text()").toString();
+        if (StringUtils.startsWith(firstItem, "统一社会信用代码")) {
+            page.putField(div.xpath("//li[1]/label/text()").toString(), div.xpath("//li[1]/text()"));
+            page.putField(div.xpath("//li[2]/label/text()").toString(), div.xpath("//li[2]/text()"));
+            page.putField(div.xpath("//li[3]/label/text()").toString(), div.xpath("//li[3]/text()"));
+            page.putField(div.xpath("//li[4]/label/text()").toString(), div.xpath("//li[4]/text()"));
+            page.putField(div.xpath("//li[5]/label/text()").toString(), div.xpath("//li[5]/a/text()"));
+            page.putField(div.xpath("//li[6]/label/text()").toString(), div.xpath("//li[6]/text()"));
+            page.putField(div.xpath("//li[7]/label/text()").toString(), div.xpath("//li[7]/text()"));
+            page.putField(div.xpath("//li[8]/label/text()").toString(), div.xpath("//li[8]/text()"));
+            page.putField(div.xpath("//li[9]/label/text()").toString(), div.xpath("//li[9]/text()"));
+        } else {
+            page.putField(div.xpath("//li[1]/label/text()").toString(), div.xpath("//li[1]/text()"));
+            page.putField(div.xpath("//li[2]/label/text()").toString(), div.xpath("//li[2]/text()"));
+            page.putField(div.xpath("//li[3]/label/text()").toString(), div.xpath("//li[3]/text()"));
+            page.putField(div.xpath("//li[4]/label/text()").toString(), div.xpath("//li[4]/text()"));
+            page.putField(div.xpath("//li[5]/label/text()").toString(), div.xpath("//li[5]/text()"));
+            page.putField(div.xpath("//li[6]/label/text()").toString(), div.xpath("//li[6]/a/text()"));
+            page.putField(div.xpath("//li[7]/label/text()").toString(), div.xpath("//li[7]/text()"));
+            page.putField(div.xpath("//li[8]/label/text()").toString(), div.xpath("//li[8]/text()"));
+            page.putField(div.xpath("//li[9]/label/text()").toString(), div.xpath("//li[9]/text()"));
+            page.putField(div.xpath("//li[10]/label/text()").toString(), div.xpath("//li[10]/text()"));
+            page.putField(div.xpath("//li[11]/label/text()").toString(), div.xpath("//li[11]/text()"));
+            page.putField(div.xpath("//li[12]/label/text()").toString(), div.xpath("//li[12]/text()"));
+        }
+
+        page.putField("公司简介",html.xpath("//*[@id=\"textShowMore\"]/text()"));
+
+
     }
 
     @Override
     public Site getSite() {
-        if (site == null) {
-            site = Site.me().setDomain("www.qichacha.com")
-                    .addStartUrl("http://www.qichacha.com/firm_d3c640fcce1a1638b8c8f491e54427d9")
-                    .setUserAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_2) AppleWebKit/537.31 (KHTML, like Gecko) Chrome/26.0.1410.65 Safari/537.31");
-        }
-        return site;
+        return this.site;
     }
 
     public static void main(String[] args) {
-        Spider.create(new QichachaProcessor())
-                .run();
+        Spider.create(new QichachaProcessor()).addUrl("http://www.qichacha.com/firm_2abadeb7044eed1a17f3202940308109.shtml")
+                .addUrl("http://www.qichacha.com/firm_a0bd2d9eb184996fcb42c722534c6993")
+                .addPipeline(new ConsolePipeline()).addPipeline(new LocalFilePipeline("/data/html")).run();
     }
 }
