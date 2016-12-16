2a6198c9488f178091323d643944df4ce36d18da
Ckex.zha <ckex868@vip.qq.com>
2016-12-16 12:36:16 +0800

    change task

--- data-sync/src/main/java/com/mljr/sync/task/AbstractTask.java
+++ data-sync/src/main/java/com/mljr/sync/task/AbstractTask.java
@@ -3,12 +3,17 @@
  */
 package com.mljr.sync.task;
 
+import java.util.Hashtable;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicReference;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.alibaba.druid.sql.dialect.oracle.ast.clause.ModelClause.ReturnRowsClause;
 import com.google.common.base.Stopwatch;
 
 /**
@@ -20,23 +25,57 @@ public abstract class AbstractTask implements Runnable {
 
 	protected static transient Logger logger = LoggerFactory.getLogger(AbstractTask.class);
 
+	private static final Map<String, AtomicReference<Status>> RUN_TASK = new Hashtable<>();
+
+	private enum Status {
+		runing, finished
+	}
+
+	private String name;
+
 	public AbstractTask() {
 		super();
 	}
 
 	abstract void execute();
 
+	public synchronized void setName(String name) {
+		this.name = name;
+		RUN_TASK.put(name, new AtomicReference<>(Status.finished));
+	}
+
 	@Override
 	public void run() {
-		Stopwatch watch = Stopwatch.createStarted();
 		try {
+			boolean isNext = checkRunning();
+			if (!isNext) {
+				logger.warn(name + ", Task is running... ");
+				return;
+			}
+			Stopwatch watch = Stopwatch.createStarted();
 			execute();
 			watch.stop();
 			logger.info("execute task, use time:" + watch.elapsed(TimeUnit.MILLISECONDS));
 		} catch (Exception ex) {
 			ex.printStackTrace();
 			logger.error("execute task error. " + ExceptionUtils.getStackTrace(ex));
+		} finally {
+			finishedTask();
 		}
+	}
+
+	private synchronized void finishedTask() {
+		if (StringUtils.isBlank(name)) {
+			return;
+		}
+		RUN_TASK.get(name).set(Status.finished);
+	}
+
+	private synchronized boolean checkRunning() {
+		if (StringUtils.isBlank(name)) {
+			return true;
+		}
+		return RUN_TASK.get(name).compareAndSet(Status.finished, Status.runing);
 	}
 
 }
