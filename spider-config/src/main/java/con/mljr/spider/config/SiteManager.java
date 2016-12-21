1808c16e222020c9e3c9cadbc5b4bab7fd018c90
chi.song <chi.song@mljr.com>
2016-12-21 20:41:41 +0800

    Merge branch 'develop' of gitlab.youjie.com:no010/spider into develop
    # Conflicts:
    #	mljr-spider/src/main/java/com/mljr/spider/scheduler/manager/Manager.java

--- spider-config/src/main/java/con/mljr/spider/config/SiteManager.java
+++ spider-config/src/main/java/con/mljr/spider/config/SiteManager.java
@@ -4,12 +4,7 @@
 package con.mljr.spider.config;
 
 import com.google.common.base.Joiner;
-import org.apache.curator.RetryPolicy;
-import org.apache.curator.framework.CuratorFramework;
-import org.apache.curator.framework.CuratorFrameworkFactory;
-import org.apache.curator.retry.ExponentialBackoffRetry;
-import org.apache.zookeeper.CreateMode;
-import org.apache.zookeeper.data.Stat;
+import org.I0Itec.zkclient.ZkClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import us.codecraft.webmagic.Site;
@@ -23,16 +18,7 @@ import java.util.List;
 public class SiteManager {
     protected transient final Logger logger = LoggerFactory.getLogger(SiteManager.class);
 
-    private static CuratorFramework client = null;
-
-    public SiteManager() {
-        if (client == null) {
-            String connectString = "127.0.0.1:2181";
-            RetryPolicy retryPolicy = new ExponentialBackoffRetry(3000, 3);
-            client = CuratorFrameworkFactory.newClient(connectString, retryPolicy);
-            client.start();
-        }
-    }
+    private static ZkClient client = ZkUtils.getZkClient();
 
     public DynamicConfig getSiteByDomain(String domain) {
         return new DynamicConfig();
@@ -48,7 +34,7 @@ public class SiteManager {
                     "10.9.88.4", "10.9.136.160", "10.9.145.53", "10.9.156.231",
                     "10.9.154.167", "10.9.169.120", "10.9.180.171"};
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
         SiteManager siteManager = new SiteManager();
         siteManager.writeAllSiteConfig();
     }
@@ -66,13 +52,11 @@ public class SiteManager {
 
         try {
             String CONFIG_PATH = Joiner.on("/").join("/config", ip, domain);
-            client.create()
-                    .creatingParentsIfNeeded()//如果父节点不存在，则创建
-                    .withMode(CreateMode.PERSISTENT)
-                    .forPath(CONFIG_PATH);
+            System.out.println(CONFIG_PATH);
 
-            Stat stat = client.setData().forPath(CONFIG_PATH, bytes);
-            System.out.println(stat.getDataLength() + "---------------------");
+            ZkUtils.createPath(CONFIG_PATH);
+
+            client.writeData(CONFIG_PATH,bytes);
         } catch (Exception e) {
             e.printStackTrace();
             logger.error("zookeeper error", e);
