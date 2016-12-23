581c52b7f387c3ef61d051e9cf9d29b7a392d183
chi.song <chi.song@mljr.com>
2016-12-23 14:19:58 +0800

    Merge branch 'develop' of gitlab.youjie.com:no010/spider into develop
    # Conflicts:
    #	mljr-spider/src/main/java/com/mljr/spider/scheduler/manager/Manager.java

--- spider-monitor/src/main/java/com.mljr.monitor/MonitorMain.java
+++ spider-monitor/src/main/java/com.mljr.monitor/MonitorMain.java
@@ -1,13 +1,23 @@
 package com.mljr.monitor;
 
-import org.springframework.boot.*;
-import org.springframework.boot.autoconfigure.*;
-import org.springframework.stereotype.*;
-import org.springframework.web.bind.annotation.*;
+import com.google.common.base.Function;
+import com.mljr.redis.RedisClient;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import redis.clients.jedis.Jedis;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
 @Controller
 @EnableAutoConfiguration
 public class MonitorMain {
+
+    private RedisClient redisClient = new RedisClient("127.0.0.1", 6379, 2000, 100, 10, 1000);
 
     @RequestMapping("/")
     @ResponseBody
@@ -17,5 +27,23 @@ public class MonitorMain {
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(MonitorMain.class, args);
+    }
+
+    @RequestMapping("/statusCode")
+    @ResponseBody
+    List<String> statusCode() {
+        List<String> list = redisClient.use(new Function<Jedis, List<String>>() {
+
+            @Override
+            public List<String> apply(Jedis jedis) {
+                List<String> allList = new ArrayList<>();
+                Set<String> set = jedis.keys("status-code-*");
+                for (String key : set) {
+                    allList.addAll(jedis.lrange(key, 0, 0));
+                }
+                return allList;
+            }
+        });
+        return list;
     }
 }
