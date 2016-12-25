b443172b135e0ce4ed11c5e6b927ae2fe6f44ebf
chi.song <chi.song@mljr.com>
2016-12-25 16:27:43 +0800

    Merge branch 'develop' of gitlab.youjie.com:no010/spider into develop
    # Conflicts:
    #	mljr-spider/src/main/java/com/mljr/spider/scheduler/manager/Manager.java

--- spider-monitor/src/main/java/com.mljr.monitor/controller/StatusCodeController.java
+++ spider-monitor/src/main/java/com.mljr.monitor/controller/StatusCodeController.java
@@ -1,19 +1,14 @@
 package com.mljr.monitor.controller;
 
-import com.alibaba.fastjson.JSON;
-import com.google.common.base.Function;
 import com.mljr.entity.MonitorData;
-import com.mljr.redis.RedisClient;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import com.mljr.monitor.service.StatusCodeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-import redis.clients.jedis.Jedis;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
 
 /**
  * Created by songchi on 16/12/23.
@@ -21,27 +16,14 @@ import java.util.Set;
 @Controller
 public class StatusCodeController {
 
-    private RedisClient redisClient = new RedisClient("127.0.0.1", 6379, 2000, 100, 10, 1000);
+    @Autowired
+    private StatusCodeService statusCodeService;
 
     @RequestMapping("/statusCode")
     public ModelAndView statusCode() {
-        List<String> jsonList = redisClient.use(new Function<Jedis, List<String>>() {
 
-            @Override
-            public List<String> apply(Jedis jedis) {
-                List<String> allList = new ArrayList<>();
-                Set<String> set = jedis.keys("status-code-*");
-                for (String key : set) {
-                    allList.addAll(jedis.lrange(key, 0, 0));
-                }
-                return allList;
-            }
-        });
-        List<MonitorData> dataList = new ArrayList<>();
-        for (String jsonStr : jsonList) {
-            MonitorData data = JSON.parseObject(jsonStr, MonitorData.class);
-            dataList.add(data);
-        }
+        List<String> jsonList = statusCodeService.getLatestRecord();
+        List<MonitorData> dataList = statusCodeService.transferToObject(jsonList);
 
         ModelAndView mav = new ModelAndView("statusCode");
         mav.addObject("dataList", dataList);
@@ -50,17 +32,9 @@ public class StatusCodeController {
 
     @RequestMapping("/detail/{domain}/other")
     public ModelAndView detail(@PathVariable("domain") String domain) {
-        List<String> jsonList = redisClient.use(new Function<Jedis, List<String>>() {
-            @Override
-            public List<String> apply(Jedis jedis) {
-                return jedis.lrange("status-code-" + domain, 0, 100);
-            }
-        });
-        List<MonitorData> dataList = new ArrayList<>();
-        for (String jsonStr : jsonList) {
-            MonitorData data = JSON.parseObject(jsonStr, MonitorData.class);
-            dataList.add(data);
-        }
+
+        List<String> jsonList = statusCodeService.getRecordByDomain(domain);
+        List<MonitorData> dataList = statusCodeService.transferToObject(jsonList);
 
         ModelAndView mav = new ModelAndView("statusCode");
         mav.addObject("dataList", dataList);
