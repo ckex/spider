7627fd3a01e6c108d94726e2175e626996fa40b5
chi.song <chi.song@mljr.com>
2016-12-26 22:39:44 +0800

    服务器监控

--- mljr-spider/src/main/java/com/mljr/spider/main/Main.java
+++ mljr-spider/src/main/java/com/mljr/spider/main/Main.java
@@ -28,8 +28,8 @@ public class Main {
 	 */
 	public static void main(String[] args) throws Exception {
 		logger.info("start...");
-//		Manager manager = new Manager();
-//		manager.run();
+		Manager manager = new Manager();
+		manager.run();
 		ZkScheduler zkScheduler =new ZkScheduler();
 		zkScheduler.startZkMonitorTask();
 		synchronized (Main.class) {
