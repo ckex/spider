2a9cca872e854873107871c8f656aded2991bed4
chi.song <chi.song@mljr.com>
2016-12-27 17:37:26 +0800

    change sougo

--- mljr-spider/src/main/java/com/mljr/spider/processor/SogouMobileProcessor.java
+++ mljr-spider/src/main/java/com/mljr/spider/processor/SogouMobileProcessor.java
@@ -11,13 +11,17 @@ import us.codecraft.webmagic.selector.Selectable;
  */
 public class SogouMobileProcessor extends AbstractPageProcessor {
 
-    private Site site = Site.me()
+    private static  final Site site = Site.me()
             .setDomain("sogou.com") //此字段在生成文件时用到
             .setSleepTime(5000)
             .setRetrySleepTime(4200)
             .setRetryTimes(3)
             .setUserAgent(
                     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36");
+
+    public SogouMobileProcessor() {
+        super(site);
+    }
 
     @Override
     public void process(Page page) {
@@ -31,8 +35,4 @@ public class SogouMobileProcessor extends AbstractPageProcessor {
         page.putField("",page.getHtml());
     }
 
-    @Override
-    public Site getSite() {
-        return site;
-    }
 }
