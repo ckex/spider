e7c8f1654ba247733a158819cb45727abd93e6c2
chi.song <chi.song@mljr.com>
2016-12-27 09:53:55 +0800

    优化

--- spider-monitor/src/main/java/com.mljr.monitor/controller/ZkConfigController.java
+++ spider-monitor/src/main/java/com.mljr.monitor/controller/ZkConfigController.java
@@ -2,10 +2,11 @@ package com.mljr.monitor.controller;
 
 import com.mljr.zk.ZkUtils;
 import org.I0Itec.zkclient.ZkClient;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 /**
  * Created by songchi on 16/12/23.
@@ -13,10 +14,12 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class ZkConfigController {
     ZkClient zkClient = ZkUtils.getZkClient();
+
     @RequestMapping("/zk")
     ModelAndView zkConfig() {
+        List<String> dataList = zkClient.getChildren("/servers");
         ModelAndView mav = new ModelAndView("index");
-        mav.addObject("dataList", "ddd");
+        mav.addObject("dataList", dataList);
         return mav;
     }
 }
