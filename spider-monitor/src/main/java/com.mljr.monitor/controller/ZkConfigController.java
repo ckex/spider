11cfddc6d63f3847c4d6ef546cfc8b39bf762b13
chi.song <chi.song@mljr.com>
2016-12-27 16:09:33 +0800

    优化

--- spider-monitor/src/main/java/com.mljr.monitor/controller/ZkConfigController.java
+++ spider-monitor/src/main/java/com.mljr.monitor/controller/ZkConfigController.java
@@ -1,25 +1,25 @@
-package com.mljr.monitor.controller;
-
-import com.mljr.zk.ZkUtils;
-import org.I0Itec.zkclient.ZkClient;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
-
-import java.util.List;
-
-/**
- * Created by songchi on 16/12/23.
- */
-@Controller
-public class ZkConfigController {
-    ZkClient zkClient = ZkUtils.getZkClient();
-
-    @RequestMapping("/zk")
-    ModelAndView zkConfig() {
-        List<String> dataList = zkClient.getChildren("/servers");
-        ModelAndView mav = new ModelAndView("index");
-        mav.addObject("dataList", dataList);
-        return mav;
-    }
-}
+//package com.mljr.monitor.controller;
+//
+//import com.mljr.zk.ZkUtils;
+//import org.I0Itec.zkclient.ZkClient;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.servlet.ModelAndView;
+//
+//import java.util.List;
+//
+///**
+// * Created by songchi on 16/12/23.
+// */
+//@Controller
+//public class ZkConfigController {
+//    ZkClient zkClient = ZkUtils.getZkClient();
+//
+//    @RequestMapping("/zk")
+//    ModelAndView zkConfig() {
+//        List<String> dataList = zkClient.getChildren("/servers");
+//        ModelAndView mav = new ModelAndView("index");
+//        mav.addObject("dataList", dataList);
+//        return mav;
+//    }
+//}
