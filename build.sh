b32ac1c069ff288546f056dbe3645b844dba5b8f
Ckex.zha <ckex868@vip.qq.com>
2016-12-27 12:57:08 +0800

    change script

--- build.sh
+++ build.sh
@@ -5,23 +5,27 @@
 
 # ./build spider
 # ./build data-sync
+# ./build.sh spider-monitor
 
 function build_spider(){
 	echo "build spider"
-	mvn clean
-	mvn -T1C -P production -pl mljr-common,umq-client,spider-config,grpc,mljr-spider package -Dmaven.test.skip -U -X
+	model="mljr-common,umq-client,spider-config,grpc,mljr-spider"
+	mvn -pl $model clean
+	mvn -T1C -P production -pl $model package -Dmaven.test.skip -U -X
 }
 
 function build_data_sync(){
 	echo "build data-sync"
-	mvn clean 
-	mvn -T1C -P production -pl mljr-common,umq-client,mljr-dao,data-sync package -Dmaven.test.skip -U -X
+	model="mljr-common,umq-client,mljr-dao,data-sync"
+	mvn -pl $model clean 
+	mvn -T1C -P production -pl $model package -Dmaven.test.skip -U -X
 }
 
 function build_spider_monitor(){
 	echo "build spider monitor"
-	mvn clean 
-	mvn -T1C -P production -pl mljr-common,umq-client,spider-config,spider-monitor package -Dmaven.test.skip -U -X
+	model="mljr-common,umq-client,spider-config,spider-monitor" 
+	mvn -pl $model clean 
+	mvn -T1C -P production -pl $model package -Dmaven.test.skip -U -X
 }
 
 case $1 in
