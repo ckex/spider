0fdb6f5d1ff3f62cbbbc136052fc3ff64c35ea4c
chi.song <chi.song@mljr.com>
2016-12-28 14:21:50 +0800

    排序

--- spider-monitor/src/main/java/com/mljr/monitor/controller/StatusCodeController.java
+++ spider-monitor/src/main/java/com/mljr/monitor/controller/StatusCodeController.java
@@ -1,7 +1,10 @@
 package com.mljr.monitor.controller;
 
+import com.google.common.collect.ComparisonChain;
 import com.mljr.entity.MonitorData;
 import com.mljr.monitor.service.StatusCodeService;
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,6 +14,7 @@ import org.springframework.web.servlet.ModelAndView;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.function.ToIntFunction;
 
 /**
  * Created by songchi on 16/12/23.
@@ -26,13 +30,9 @@ public class StatusCodeController {
 
         List<String> jsonList = statusCodeService.getLatestRecord();
         List<MonitorData> dataList = statusCodeService.transferToObject(jsonList);
-        Collections.sort(dataList, new Comparator<MonitorData>() {
-            @Override
-            public int compare(MonitorData o1, MonitorData o2) {
-                return o1.getDomain().compareTo(o2.getDomain());
-            }
-        });
-
+        if(CollectionUtils.isNotEmpty(dataList)){
+            dataList.sort(Comparator.comparing(MonitorData::getDomain).thenComparing(MonitorData::getServerIp));
+        }
         ModelAndView mav = new ModelAndView("statusCode");
         mav.addObject("dataList", dataList);
         mav.addObject("totolCount", statusCodeService.totolCount(dataList));
