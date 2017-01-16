12fa2dc2a49b41ff7dab58bc59a5b5b13fceeaf8
chi.song <chi.song@mljr.com>
2017-01-16 17:40:21 +0800

    sync qqNumber

--- data-sync/src/main/java/com/mljr/sync/task/QqNumberTask.java
+++ data-sync/src/main/java/com/mljr/sync/task/QqNumberTask.java
@@ -6,12 +6,9 @@ package com.mljr.sync.task;
 import com.mljr.sync.service.QqNumberService;
 import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.config.ConfigurableBeanFactory;
-import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
 @Service
-@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class QqNumberTask extends AbstractTask {
 
 	@Autowired
