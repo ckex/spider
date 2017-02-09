5be67ea33a3e602e5feff3c4b5f9463d49e4970b
Ckex.zha <ckex868@vip.qq.com>
2017-02-09 10:14:22 +0800

    change test

--- mljr-spider/src/test/java/com/mljr/spider/json/GsonTests.java
+++ mljr-spider/src/test/java/com/mljr/spider/json/GsonTests.java
@@ -33,15 +33,17 @@ public class GsonTests {
 	 */
 	public static void main(String[] args) {
 		String jsonText = readJsonText();
+		System.out.println(jsonText.length());
 		Gson gson = new GsonBuilder().create();
-		gson.fromJson(jsonText, Map.class);
+		// gson.fromJson(jsonText, Map.class);
 		// JSON.parse(jsonText);
-		// JSONObject jsonObject = JSON.parseObject(jsonText);
+		JSONObject jsonObject = JSON.parseObject(jsonText);
 
 	}
 
 	public static String readJsonText() {
 		String filePath = "/Users/ckex/Desktop/gps.json";
+		filePath = "/Users/ckex/Desktop/1485063511129-part-0000.json";
 		try {
 			String encoding = "UTF-8";
 			File file = new File(filePath);
