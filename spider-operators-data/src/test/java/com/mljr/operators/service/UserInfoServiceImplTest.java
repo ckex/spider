906e55e1d4c2fefb3ca764ee972e39210b24e8c0
gaoxi <gaoxi1534@gmail.com>
2017-02-22 18:49:07 +0800

    add insert batch

--- spider-operators-data/src/test/java/com/mljr/operators/service/UserInfoServiceImplTest.java
+++ spider-operators-data/src/test/java/com/mljr/operators/service/UserInfoServiceImplTest.java
@@ -1,22 +1,16 @@
 package com.mljr.operators.service;
 
-import com.mljr.operators.SpringBootOperatorsMain;
 import com.mljr.operators.entity.model.operators.UserInfo;
 import com.mljr.operators.service.primary.operators.IUserInfoService;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.util.Assert;
 
 /**
  * @author gaoxi
  * @time 2017/2/22
  */
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = SpringBootOperatorsMain.class)
-public class UserInfoServiceImplTest {
+public class UserInfoServiceImplTest extends BaseTest {
 
     @Autowired
     private IUserInfoService userInfoService;
