c74fae4938afa7f9edc9832c676fff386861b00c
chi.song <chi.song@mljr.com>
2017-02-24 15:44:50 +0800

    新增批量接口

--- spider-operators-data/src/main/java/com/mljr/operators/dao/primary/operators/FlowInfoMapper.java
+++ spider-operators-data/src/main/java/com/mljr/operators/dao/primary/operators/FlowInfoMapper.java
@@ -4,7 +4,9 @@ import com.mljr.operators.dao.BaseMapper;
 import com.mljr.operators.entity.model.operators.FlowInfo;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
 @Mapper
 public interface FlowInfoMapper extends BaseMapper<FlowInfo, Long> {
-
+    int insertByBatch(List<FlowInfo> list);
 }
