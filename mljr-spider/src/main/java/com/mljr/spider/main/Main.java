497c2ae4e41d2440df1c2a1cd15762880bcbef61
fulin <lin.fu01@mljr.com>
2017-02-27 12:26:15 +0800

    Merge branch 'develop' of gitlab.youjie.com:no010/spider into 20170220
    # Conflicts:
    #	mljr-dao/src/main/resources/resource/jdbc.properties
    360代码格式一致性

--- mljr-spider/src/main/java/com/mljr/spider/main/Main.java
+++ mljr-spider/src/main/java/com/mljr/spider/main/Main.java
@@ -19,40 +19,40 @@ import java.util.concurrent.TimeUnit;
  */
 public class Main {
 
-    protected static transient Logger logger = LoggerFactory.getLogger(Main.class);
+  protected static transient Logger logger = LoggerFactory.getLogger(Main.class);
 
-    private static ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
+  private static ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
 
-    public Main() {
-        super();
+  public Main() {
+    super();
+  }
+
+  /**
+   * @param args
+   * @throws Exception
+   */
+  public static void main(String[] args) throws Exception {
+    logger.info("start...");
+    startLBSTask();
+    Manager manager = new Manager();
+    manager.run();
+    ZkScheduler zkScheduler = new ZkScheduler();
+    zkScheduler.startZkMonitorTask();
+    synchronized (Main.class) {
+      try {
+        Main.class.wait();
+      } catch (InterruptedException e) {
+      }
     }
+  }
 
-    /**
-     * @param args
-     * @throws Exception
-     */
-    public static void main(String[] args) throws Exception {
-        logger.info("start...");
-        startLBSTask();
-        Manager manager = new Manager();
-        manager.run();
-        ZkScheduler zkScheduler =new ZkScheduler();
-        zkScheduler.startZkMonitorTask();
-        synchronized (Main.class) {
-            try {
-                Main.class.wait();
-            } catch (InterruptedException e) {
-            }
-        }
+  private static void startLBSTask() {
+    try {
+      // 每小时检查一次失效的Key
+      executor.scheduleWithFixedDelay(new LBSTask(), 0, 1, TimeUnit.HOURS);
+    } catch (Exception e) {
+      logger.error("start lbs task failure.", e);
     }
-
-    private static void startLBSTask() {
-        try {
-            //每小时检查一次失效的Key
-            executor.scheduleWithFixedDelay(new LBSTask(), 0, 1, TimeUnit.HOURS);
-        } catch (Exception e) {
-            logger.error("start lbs task failure.", e);
-        }
-    }
+  }
 
 }
