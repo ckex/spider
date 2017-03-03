218e3f805f56ca7837e5ca27a5b56e2269e0459e
Ckex.zha <ckex868@vip.qq.com>
2017-03-03 13:29:39 +0800

    add config

---
+++ docker_spider_startup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+function start_spider(){
+	command -v docker >/dev/null 2>&1 || { echo >&2 "Command not found: docker. please install docker."; exit 1; }
+	echo "start spider ... "
+	docker pull ucloud20:5000/ckex/spider:develop
+	docker stop spider
+	docker rm spider
+	hn=`hostname` &&  docker run --name spider --hostname="docker$hn" -v /data:/data -d ucloud20:5000/ckex/spider:develop restartws.sh
+}
+
+case $1 in
+	"start-spider")
+		start_spider
+	;;
+	*)
+		echo "wrong select" && exit 1
+	;;
+esac
+
+
+
+## 
+#?/etc/docker/daemon.json
+# {
+# "registry-mirrors": ["http://c6823a84.m.daocloud.io"],
+# "insecure-registries":["ucloud20:5000"]
+# }
