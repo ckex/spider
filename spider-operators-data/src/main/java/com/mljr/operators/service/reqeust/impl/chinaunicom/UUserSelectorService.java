c478333d1deddf315c3df753d61306703cd5f871
gaoxi <gaoxi1534@gmail.com>
2017-03-03 13:25:14 +0800

    新增URL保存

--- spider-operators-data/src/main/java/com/mljr/operators/service/reqeust/impl/chinaunicom/UUserSelectorService.java
+++ spider-operators-data/src/main/java/com/mljr/operators/service/reqeust/impl/chinaunicom/UUserSelectorService.java
@@ -3,6 +3,7 @@ package com.mljr.operators.service.reqeust.impl.chinaunicom;
 import com.google.common.collect.Lists;
 import com.mljr.operators.common.constant.OperatorsEnum;
 import com.mljr.operators.common.constant.OperatorsUrlEnum;
+import com.mljr.operators.common.utils.DateUtil;
 import com.mljr.operators.entity.chinamobile.DatePair;
 import com.mljr.operators.entity.dto.operator.RequestInfoDTO;
 import com.mljr.operators.entity.dto.operator.RequestUrlDTO;
@@ -10,6 +11,7 @@ import com.mljr.operators.service.reqeust.AbstractRequestUrlSelectorService;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -20,11 +22,14 @@ import java.util.List;
 public class UUserSelectorService extends AbstractRequestUrlSelectorService {
 
   @Override
-  public List<RequestInfoDTO> getRequestUrl(RequestUrlDTO requestUrl) {
+  public List<RequestInfoDTO> getRequestUrl(RequestUrlDTO requestUrl, Date filterDate) {
     List<RequestInfoDTO> list = Lists.newArrayList();
-    RequestInfoDTO entity = convert(requestUrl.getMobile(), requestUrl.getIdcard(),
-        getYesterDayOfMonth(requestUrl.getStartDate()), getOperatorsUrl().getUrl());
-    list.add(entity);
+    DatePair datePair = getYesterDayOfMonth(requestUrl.getStartDate());
+    if (null == filterDate || null != filterDate && null != filterUrl(filterDate, datePair)) {
+      RequestInfoDTO entity = convert(requestUrl.getMobile(), requestUrl.getIdcard(), datePair,
+          getOperatorsUrl().getUrl());
+      list.add(entity);
+    }
     return list;
   }
 
@@ -36,5 +41,17 @@ public class UUserSelectorService extends AbstractRequestUrlSelectorService {
   @Override
   protected OperatorsUrlEnum getOperatorsUrl() {
     return OperatorsUrlEnum.CHINA_UNICOM_USER_INFO;
+  }
+
+  @Override
+  protected DatePair filterUrl(Date filterDate, DatePair datePair) {
+    LocalDate filterLocalDate = DateUtil.dateToLocalDate(filterDate);
+    LocalDate endDate =
+        DateUtil.stringToLocalDate(datePair.getEndDate(), DateUtil.PATTERN_yyyy_MM_dd);
+    if (filterLocalDate.getYear() == endDate.getYear()
+        && filterLocalDate.getMonthValue() == endDate.getMonthValue()) {
+      return null;
+    }
+    return datePair;
   }
 }
