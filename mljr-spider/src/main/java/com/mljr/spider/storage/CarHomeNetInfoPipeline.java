a0f38dfcb0dcd8b421d52e6771bdf3b363bea25b
fulin <lin.fu01@mljr.com>
2017-03-10 16:40:23 +0800

    汽车之家20170310-1640

--- mljr-spider/src/main/java/com/mljr/spider/storage/CarHomeNetInfoPipeline.java
+++ mljr-spider/src/main/java/com/mljr/spider/storage/CarHomeNetInfoPipeline.java
@@ -30,7 +30,7 @@ public class CarHomeNetInfoPipeline implements Pipeline {
     try {
       sendRmq(JSON.toJSONString(listmap));
     } catch (Exception e) {
-      logger.error("send page data error!!!", e);
+      logger.error("send page data error!", e);
     }
   }
 
@@ -39,7 +39,7 @@ public class CarHomeNetInfoPipeline implements Pipeline {
     AMQP.BasicProperties.Builder builder = new AMQP.BasicProperties.Builder();
     builder.contentEncoding(BasicConstant.UTF8).contentType(BasicConstant.TEXT_PLAIN).deliveryMode(1).priority(0);
     try {
-      RabbitmqClient.publishMessage(channel, "", "car_test", builder.build(), data.getBytes(Charsets.UTF_8));
+      RabbitmqClient.publishMessage(channel, "", "autohome_result", builder.build(), data.getBytes(Charsets.UTF_8));
     } catch (Exception e) {
       logger.error("send page data error!", e);
     } finally {
