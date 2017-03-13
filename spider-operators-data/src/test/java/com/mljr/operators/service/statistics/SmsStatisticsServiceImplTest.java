6b69799f4d4e642831e34f5eecd5d1a6993f0671
gaoxi <gaoxi1534@gmail.com>
2017-03-13 12:10:27 +0800

    add sms statistics test

---
+++ spider-operators-data/src/test/java/com/mljr/operators/service/statistics/SmsStatisticsServiceImplTest.java
@@ -0,0 +1,35 @@
+package com.mljr.operators.service.statistics;
+
+import com.mljr.operators.entity.vo.statistics.SmsStatisticsVO;
+import com.mljr.operators.service.BaseTest;
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+/**
+ * Created by gaoxi
+ *
+ * @Time: 2017/3/13 下午12:01
+ */
+public class SmsStatisticsServiceImplTest extends BaseTest {
+
+  @Autowired
+  private ISmsStatisticsService smsStatisticsService;
+
+    @Override
+    public void testInit() {
+        super.testInit();
+        Assert.assertNotNull(smsStatisticsService);
+    }
+
+    @Test
+  public void testGetTimeBySmsType() {
+
+      List<SmsStatisticsVO> list = smsStatisticsService.getTimeBySmsType(10);
+
+        System.out.println();
+
+  }
+}
