33eb0303d4bec48b39f59a47f27b66513f1e4280
gaoxi <gaoxi1534@gmail.com>
2017-03-13 11:37:25 +0800

    add statistics vo

---
+++ spider-operators-data/src/main/java/com/mljr/operators/entity/vo/statistics/SmsStatisticsVO.java
@@ -0,0 +1,9 @@
+package com.mljr.operators.entity.vo.statistics;
+
+/**
+ * Created by gaoxi
+ *
+ * @Time: 2017/3/13 上午11:36
+ */
+public class SmsStatisticsVO {
+}
--- spider-operators-data/src/main/java/com/mljr/operators/service/statistics/ISmsStatisticsService.java
+++ spider-operators-data/src/main/java/com/mljr/operators/service/statistics/ISmsStatisticsService.java
@@ -3,6 +3,8 @@ package com.mljr.operators.service.statistics;
 /**
  * Created by songchi on 17/3/13.
  */
-public class ISmsStatisticsService {
+public interface ISmsStatisticsService {
+
+
 
 }
