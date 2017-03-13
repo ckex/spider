5b0e30c1686174165e459d8a56c3d11b1fcc3b4e
chi.song <chi.song@mljr.com>
2017-03-13 12:10:38 +0800

    tongji

--- spider-operators-data/src/main/java/com/mljr/operators/entity/vo/statistics/SmsStatisticsVO.java
+++ spider-operators-data/src/main/java/com/mljr/operators/entity/vo/statistics/SmsStatisticsVO.java
@@ -1,9 +1,16 @@
 package com.mljr.operators.entity.vo.statistics;
 
+import java.util.Date;
+
 /**
  * Created by gaoxi
  *
  * @Time: 2017/3/13 上午11:36
  */
 public class SmsStatisticsVO {
+    private Date maxCallOutDate;
+    private Date minCallOUtDate;
+
+    private Date maxCallInDate;
+    private Date minCallInDate;
 }
