ee8dee4d0e5c5243ece5ea3952ccd756f002ee68
gaoxi <gaoxi1534@gmail.com>
2017-03-14 10:29:25 +0800

    delete jobs and add package

--- spider-operators-data/src/main/java/com/mljr/operators/SpringBootOperatorsMain.java
+++ spider-operators-data/src/main/java/com/mljr/operators/SpringBootOperatorsMain.java
@@ -9,11 +9,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.ApplicationContext;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableConfigurationProperties
-@EnableScheduling
 public class SpringBootOperatorsMain {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(SpringBootOperatorsMain.class);
