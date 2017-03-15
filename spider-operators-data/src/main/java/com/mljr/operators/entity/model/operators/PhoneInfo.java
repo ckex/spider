51de3820727ea8b481085969d9d3ab43e5b1ea9c
chi.song <chi.song@mljr.com>
2017-03-15 10:41:16 +0800

    add entity

---
+++ spider-operators-data/src/main/java/com/mljr/operators/entity/model/operators/PhoneInfo.java
@@ -0,0 +1,83 @@
+package com.mljr.operators.entity.model.operators;
+
+public class PhoneInfo {
+    private Long id;
+
+    private String phone;
+
+    private String areaCode;
+
+    private String card;
+
+    private String city;
+
+    private String company;
+
+    private String province;
+
+    private String zip;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone == null ? null : phone.trim();
+    }
+
+    public String getAreaCode() {
+        return areaCode;
+    }
+
+    public void setAreaCode(String areaCode) {
+        this.areaCode = areaCode == null ? null : areaCode.trim();
+    }
+
+    public String getCard() {
+        return card;
+    }
+
+    public void setCard(String card) {
+        this.card = card == null ? null : card.trim();
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city == null ? null : city.trim();
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company == null ? null : company.trim();
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    public void setProvince(String province) {
+        this.province = province == null ? null : province.trim();
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip == null ? null : zip.trim();
+    }
+}
